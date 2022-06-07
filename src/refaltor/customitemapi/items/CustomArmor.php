0132d119c17f022d9b0c6ead704af395d51231c6
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2022-06-07 21:40:26 +0200

    Update CustomArmor.php

--- src/refaltor/customitemapi/items/CustomArmor.php
+++ src/refaltor/customitemapi/items/CustomArmor.php
@@ -1,28 +1,9 @@
 <?php
 
-/*
- *    _______           _______ _________ _______  _______ __________________ _______  _______  _______  _______ _________
- *   (  ____ \|\     /|(  ____ \\__   __/(  ___  )(       )\__   __/\__   __/(  ____ \(       )(  ___  )(  ____ )\__   __/
- *   | (    \/| )   ( || (    \/   ) (   | (   ) || () () |   ) (      ) (   | (    \/| () () || (   ) || (    )|   ) (
- *   | |      | |   | || (_____    | |   | |   | || || || |   | |      | |   | (__    | || || || (___) || (____)|   | |
- *   | |      | |   | |(_____  )   | |   | |   | || |(_)| |   | |      | |   |  __)   | |(_)| ||  ___  ||  _____)   | |
- *   | |      | |   | |      ) |   | |   | |   | || |   | |   | |      | |   | (      | |   | || (   ) || (         | |
- *   | (____/\| (___) |/\____) |   | |   | (___) || )   ( |___) (___   | |   | (____/\| )   ( || )   ( || )      ___) (___
- *   (_______/(_______)\_______)   )_(   (_______)|/     \|\_______/   )_(   (_______/|/     \||/     \||/       \_______/
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Lesser General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   By: refaltor
- *   Discord: Refaltor#6969
- */
+namespace core\items\itemsTemplates;
 
-declare(strict_types=1);
-
-namespace refaltor\customitemapi\items;
-
+use pocketmine\block\Block;
+use pocketmine\crafting\ShapedRecipe;
 use pocketmine\item\Armor;
 use pocketmine\item\ArmorTypeInfo;
 use pocketmine\item\ItemIdentifier;
@@ -30,28 +11,9 @@ use pocketmine\item\ItemUseResult;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\player\Player;
-use pocketmine\Server;
-use refaltor\customitemapi\traits\UtilsTrait;
 
-class CustomArmor extends Armor
+class ArmorItem extends Armor
 {
-
-    use UtilsTrait;
-
-    protected $lore  = [''];
-
-    const ARMOR_CLASS = [
-        "gold",
-        "none",
-        "leather",
-        "chain",
-        "iron",
-        "diamond",
-        "elytra",
-        "turtle",
-        "netherite"
-    ];
-
     const ARMOR_GROUP = [
         0 => 'itemGroup.name.helmet',
         1 => 'itemGroup.name.chestplate',
@@ -59,41 +21,38 @@ class CustomArmor extends Armor
         3 => 'itemGroup.name.boots'
     ];
 
-    private string $textureName;
-    private string $armorClass;
+    const ARMOR_ENCHANT = [
+        0 => 'armor_helmet',
+        1 => 'armor_torso',
+        2 => 'armor_legs',
+        3 => 'armor_feet'
+    ];
 
-    public function __construct(
-        ItemIdentifier $identifier,
-        string $name,
-        ArmorTypeInfo $info,
-        string $textureName,
-        string $classArmor = 'diamond'
-    )
+    const ARMOR_WEARABLE = [
+        0 => 'slot.armor.head',
+        1 => 'slot.armor.chest',
+        2 => 'slot.armor.legs',
+        3 => 'slot.armor.feet'
+    ];
+    protected $lore = [];
+    private string $texture_path;
+    private ?ShapedRecipe $recipe;
+
+    public function __construct(ItemIdentifier $identifier, string $name, ArmorTypeInfo $info, string $texture_path, ?ShapedRecipe $recipe = null, array $lore = [])
     {
-        if (!in_array($classArmor, self::ARMOR_CLASS)) {
-            Server::getInstance()->getLogger()->error("[CustomItemAPI] Error //: Item" . $this->getId() . ":" . $this->getMeta()) . ", armor class not found.";
-        }
-        $this->textureName = $textureName;
-        $this->armorClass = $classArmor;
+        $this->texture_path = $texture_path;
+        $this->recipe = $recipe;
+        $this->lore = $lore;
         parent::__construct($identifier, $name, $info);
     }
 
-    public function onClickAir(Player $player, Vector3 $directionVector) : ItemUseResult{
-        $existing = $player->getArmorInventory()->getItem($this->getArmorSlot());
-        $thisCopy = clone $this;
-        $new = $thisCopy->pop();
-        $player->getArmorInventory()->setItem($this->getArmorSlot(), $new);
-        if($thisCopy->getCount() === 0){
-            $player->getInventory()->setItemInHand($existing);
-        }else{ //if the stack size was bigger than 1 (usually won't happen, but might be caused by plugins
-            $player->getInventory()->setItemInHand($thisCopy);
-            $player->getInventory()->addItem($existing);
-        }
-        // TODO: add sound equip.
-        return ItemUseResult::SUCCESS();
+    public function onInteractBlock(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector): ItemUseResult
+    {
+        return parent::onInteractBlock($player, $blockReplace, $blockClicked, $face, $clickVector);
     }
 
-    public function getComponents(): CompoundTag {
+    public function getNbt(): CompoundTag
+    {
         return CompoundTag::create()
             ->setTag("components", CompoundTag::create()
                 ->setTag("minecraft:durability", CompoundTag::create()
@@ -101,11 +60,11 @@ class CustomArmor extends Armor
                     ->setInt("max_durability", $this->getMaxDurability())
                 )
                 ->setTag("minecraft:armor", CompoundTag::create()
-                    ->setString("texture_type", $this->getArmorClass())
+                    ->setString("texture_type", 'diamond')
                     ->setInt("protection", $this->getDefensePoints())
                 )
                 ->setTag("minecraft:wearable", CompoundTag::create()
-                    ->setInt("slot", $this->getArmorSlot() + 2)
+                    ->setString("slot", self::ARMOR_WEARABLE[$this->getArmorSlot()])
                     ->setByte("dispensable", 1)
                 )
                 ->setTag("item_properties", CompoundTag::create()
@@ -119,28 +78,19 @@ class CustomArmor extends Armor
                     ->setInt("creative_category", 3)
                     ->setString("creative_group", self::ARMOR_GROUP[$this->getArmorSlot()])
                     ->setTag("minecraft:icon", CompoundTag::create()
-                        ->setString("texture", $this->getTextureName())
-                        ->setString("legacy_id", "custom:" . $this->name)
+                        ->setString("texture", $this->texture_path)
+                        ->setString("legacy_id", "goldrush:" . $this->name)
                     )
                 )
             )
             ->setShort("minecraft:identifier", $this->getId() + ($this->getId() > 0 ? 5000 : -5000))
             ->setTag("minecraft:display_name", CompoundTag::create()
-                ->setString("value", $this->checkName($this->getVanillaName()))
+                ->setString("value", "item." . strtolower($this->name) . "name")
             )
             ->setTag("minecraft:on_use", CompoundTag::create()
                 ->setByte("on_use", 1)
             )->setTag("minecraft:on_use_on", CompoundTag::create()
                 ->setByte("on_use_on", 1)
             );
-    }
-
-
-    public function getTextureName(): string {
-       return $this->textureName;
-    }
-
-    public function getArmorClass(): string {
-        return $this->armorClass;
     }
 }
